3160d80f70b437cb64195da701ce4685bf7191a1
cozimo-davinci <cozimodeaudiatore@gmail.com>
2024-09-10 23:23:17 -0400

    last changes

--- exercises.js
+++ exercises.js
@@ -1,45 +1,45 @@
 // // Exercise 1
-// function greeter(myArray, counter) {
-//     counter = 0;
-//     const greetText = "Hello, ";
-//     for (let index = 0; index < myArray.length; index++) {
-//         counter++;
-//         console.log(counter + ": " + greetText + myArray[index]);
-//     }
-// }
-//
-// greeter(['Randy Savage', 'Ric Flair', 'Hulk Hogan'], 3);
+function greeter(myArray, counter) {
+    counter = 0;
+    const greetText = "Hello, ";
+    for (let index = 0; index < myArray.length; index++) {
+        counter++;
+        console.log(counter + ": " + greetText + myArray[index]);
+    }
+}
+
+greeter(['Randy Savage', 'Ric Flair', 'Hulk Hogan'], 3);
 
 // Exercise 2
-// function capitalizer(input) {
-//
-//     const [initial, ...rest] = input;
-//     return initial.toUpperCase() + rest.join('');
-// }
-//
-// console.log(capitalizer('fooBar'));
-// console.log(capitalizer('nodeJs'));
+function capitalizer(input) {
+
+    const [initial, ...rest] = input;
+    return initial.toUpperCase() + rest.join('');
+}
+
+console.log(capitalizer('fooBar'));
+console.log(capitalizer('nodeJs'));
 
 // Exercise 3
-// const colors = ['red', 'green', 'blue'];
-//
-// const capitalizedColors = colors.map(capitalizer);
-//
-// console.log(capitalizedColors);
+const colors = ['red', 'green', 'blue'];
+
+const capitalizedColors = colors.map(capitalizer);
+
+console.log(capitalizedColors);
 
 // Exercise 4
-//  let values = [1, 60, 34, 30, 20, 5]
-//
-// const filterLessThan20 = values.filter(value => value < 20);
-//  console.log(filterLessThan20)
+ let values = [1, 60, 34, 30, 20, 5]
+
+const filterLessThan20 = values.filter(value => value < 20);
+ console.log(filterLessThan20)
 
 // Exercise 5
-// let array = [1, 2, 3, 4];
-// const calculateSum = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-// const calculateProduct = array.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
-//
-// console.log(calculateSum);
-// console.log(calculateProduct);
+let array = [1, 2, 3, 4];
+const calculateSum = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+const calculateProduct = array.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+
+console.log(calculateSum);
+console.log(calculateProduct);
 
 // Exercise 6
 class Car {
